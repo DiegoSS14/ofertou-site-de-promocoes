a10ae50d10f50bc04b77251bcc704776619fb5c0
DiegoSS14 <sousadiego533@gmail.com>
2025-05-31 11:01:31 -0300

    Primeiro commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,56 @@
+import "./home.css"
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import CardProduct from "../../components/CardProduct";
+
+function Home() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function loadPorducts() {
+            const response = await api.get("https://dummyjson.com/products", {
+                params:{
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            setProducts(response.data.products.slice(0, 20));
+            console.log(response.data.products.slice(0, 20));
+        }
+        
+        loadPorducts();
+    }, [])
+
+
+    return (
+        <div className="homeContainer">
+            <section>
+                <h2>Categoria</h2>
+                <article className="lista-produtos">
+
+                    {products.map((prod) => { return(
+                        <CardProduct
+                            tags={prod.tags}
+                            titulo={prod.title}
+                            valor={prod.price} 
+                            desconto={prod.discountPercentage}
+                            img={prod.thumbnail}
+                        />
+                    )})}
+
+                    <CardProduct
+                        tags={["Beleza", "Moda"]}
+                        titulo="Produto"
+                        valor="9,99"
+                        desconto="10"
+                        img="https://neilpatel.com/wp-content/uploads/2019/07/mini-caixas-de-produtos-em-cima-de-laptop-700x468.jpeg"
+                    />
+                </article>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
